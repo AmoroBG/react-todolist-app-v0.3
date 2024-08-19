98ec98463d2d4eb08e42c263cc7cd510f4fd5356
amorobg <amorobg@gmail.com>
2024-08-19 08:13:13 +0000

    Project clean up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header heading="Todo List App" />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
