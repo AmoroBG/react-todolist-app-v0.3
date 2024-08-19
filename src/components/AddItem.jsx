36853eb2d1e92f19a43206c2bc5b4c68c5a72d24
amorobg <amorobg@gmail.com>
2024-08-19 10:52:12 +0000

    Add inputRef functionality

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const AddItem = ({ handleSubmit, newItem, setNewItem }) => {
+  const inputRef = useRef();
   return (
     <form className="add-item" onSubmit={handleSubmit}>
       <label htmlFor="add-item">Add Item</label>
@@ -9,10 +10,15 @@ const AddItem = ({ handleSubmit, newItem, setNewItem }) => {
         placeholder="Add Item..."
         required
         autoFocus
+        ref={inputRef}
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type="submit" className="add">
+      <button
+        type="submit"
+        className="add"
+        onClick={() => inputRef.current.focus()}
+      >
         Add
       </button>
     </form>
